189fd2e41f08d43d7f8bde7c22f506fedcc511bb
Cale Augustine <caleagus@gmail.com>
2024-11-21 17:38:13 -0600

    Added arrays

--- Test1/Test1.ino
+++ Test1/Test1.ino
@@ -13,44 +13,36 @@ void loop(){
   delay(2000);
   }
 
+int arr[12];
 const float R0 = 100;
 void ReadBar(int p1,int p2){
   pinMode(p1, OUTPUT);
   digitalWrite(p1,HIGH);
   
-  float res0 = analogRead(A5);
-  float res1 = analogRead(A4);
-  float res2 = analogRead(A3);
-  float res3 = analogRead(A2);
-  float res4 = analogRead(A1);
-  float res5 = analogRead(A0);
+  arr[0] = analogRead(A5);
+  arr[1] = analogRead(A4);
+  arr[2] = analogRead(A3);
+  arr[3] = analogRead(A2);
+  arr[4] = analogRead(A1);
+  arr[5] = analogRead(A0);
   digitalWrite(p1,LOW);
   pinMode(p1, INPUT);
 
   pinMode(p2, OUTPUT);
   digitalWrite(p2,HIGH);
-  float res6 = analogRead(A5);
-  float res7 = analogRead(A4);
-  float res8 = analogRead(A3);
-  float res9 = analogRead(A2);
-  float res10 = analogRead(A1);
-  float res11 = analogRead(A0);
+  arr[6] = analogRead(A5);
+  arr[7] = analogRead(A4);
+  arr[8] = analogRead(A3);
+  arr[9] = analogRead(A2);
+  arr[10] = analogRead(A1);
+  arr[11] = analogRead(A0);
   digitalWrite(p2,LOW);
   pinMode(p2, INPUT); 
+
+  PrintArr(arr);
   
-  Print(res0);
-  Print(res1);
-  Print(res2);
-  Print(res3);
-  Print(res4);
-  Print(res5);
-  Print(res6);
-  Print(res7);
-  Print(res8);
-  Print(res9);
-  Print(res10);
-  Print(res11);
   Serial.println("");
   }
 
-void Print(int var){Serial.print(var);  Serial.print(" ");}
+void PrintArr(int arr[12]){for(int i =0; i<12;i++){Serial.print(arr[i]);Serial.print(" ");}}
+//void Print(int var){Serial.print(var);  Serial.print(" ");}
